7ede16ccfd14615e48d9c67105a143cef4a3f6d0
Jesús Martínez <jesusmartinez@Jesuss-MacBook-Pro.local>
2018-07-03 18:42:54 -0500

    Adds better names to the attributes.

--- src/main/scala/knn/KNN.scala
+++ src/main/scala/knn/KNN.scala
@@ -9,24 +9,25 @@ object KNN extends App {
 
   val attributes = new Array[Attribute](8)
 
-  attributes(0) = new NominalAttribute("0")
-  attributes(1) = new NumericAttribute("1")
-  attributes(2) = new NumericAttribute("2")
-  attributes(3) = new NumericAttribute("3")
-  attributes(4) = new NumericAttribute("4")
-  attributes(5) = new NumericAttribute("5")
-  attributes(6) = new NumericAttribute("6")
-  attributes(7) = new NumericAttribute("7")
+  attributes(0) = new NominalAttribute("Sequence Name")
+  attributes(1) = new NumericAttribute("mcg")
+  attributes(2) = new NumericAttribute("gvh")
+  attributes(3) = new NumericAttribute("lip")
+  attributes(4) = new NumericAttribute("chg")
+  attributes(5) = new NumericAttribute("aac")
+  attributes(6) = new NumericAttribute("alm1")
+  attributes(7) = new NumericAttribute("alm2")
 
-  val label = new NominalAttribute("8")
+  val label = new NominalAttribute("class")
 
-  val data: AttributeDataset = read.csv(
-    this.getClass.getClassLoader.getResource("knn/ecoli.csv").toURI.getPath,
-    attributes = attributes,
-    response = Some((label, 8)))
+  val dataFileUri = this.getClass.getClassLoader.getResource("knn/ecoli.csv").toURI.getPath
+  val data: AttributeDataset = read.csv(dataFileUri, attributes = attributes, response = Some((label, 8)))
+
+  println("Sneak peek of the data:")
+  println(data)
 
   val model: KNN[Array[Double]] = knn(data.x(), data.labels(), 3)
+  val predictions = model.predict(data.x())
 
-  println(accuracy(data.labels(), model.predict(data.x())))
-
+  println(s"Model's accuracy on the training set: ${accuracy(data.labels(), predictions)}")
 }
