02b5772891e832d3f11445227ea43c3421874da0
Jesús Martínez <jesusmartinez@Jesuss-MacBook-Pro.local>
2018-07-03 21:31:45 -0500

    Adds FLA

---
+++ src/main/scala/fisher/FisherLinearDiscriminant.scala
@@ -0,0 +1,28 @@
+package fisher
+
+import smile.classification.fisher
+import smile.data.{Attribute, AttributeDataset, NominalAttribute}
+import smile.read
+import smile.validation.accuracy
+
+object FisherLinearDiscriminant extends App {
+  val attributes = new Array[Attribute](4)
+
+  attributes(0) = new NominalAttribute("Left-Weight")
+  attributes(1) = new NominalAttribute("Left-Distance")
+  attributes(2) = new NominalAttribute("Right-Weight")
+  attributes(3) = new NominalAttribute("Right-Distance")
+
+  val label = new NominalAttribute("Class Name")
+
+  val dataFileUri = this.getClass.getClassLoader.getResource("lda/balance-scale.data").toURI.getPath
+  val data: AttributeDataset = read.csv(dataFileUri, attributes = attributes, response = Some((label, 0)))
+
+  println("Sneak peek of the data:")
+  println(data)
+
+  val model = fisher(data.x(), data.labels())
+  val predictions = model.predict(data.x())
+
+  println(s"Model's accuracy on the training set: ${accuracy(data.labels(), predictions)}")
+}
